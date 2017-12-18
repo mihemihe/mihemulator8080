b866ae4809b83465afbcc6b285db90f0b72bf9b3
mihe <mihemihe@hotmail.com>
2017-12-18 16:22:06 +0400

    Update CPU disassembler with more instructions

--- mihemulator8080/CPU.cs
+++ mihemulator8080/CPU.cs
@@ -29,45 +29,48 @@ namespace mihemulator8080
             }
             for (byte position = 0; position < bytesROM.Bytes.Length; position++)
             {
-                
                 string currentByte = BitConverter.ToString(bytesROM.Bytes, position, 1);
-                string byteOffset1 = BitConverter.ToString(bytesROM.Bytes, position + 1, 1); //out of bound at the endfix
-                string byteOffset2 = BitConverter.ToString(bytesROM.Bytes, position + 2, 1);
-
+                string byte2 = BitConverter.ToString(bytesROM.Bytes, position + 1, 1); //out of bound at the endfix
+                string byte3 = BitConverter.ToString(bytesROM.Bytes, position + 2, 1);
 
                 switch (currentByte)
                 {
                     // case "01": assemblyLines.Add($""); break;
                     // Opcode	Instruction	size	flags	function
-                    case "00": assemblyLines.Add($"NOP"); break; 
-                    case "01": assemblyLines.Add($"LXI    B,#${byteOffset2}{byteOffset1}"); break;
-                    case "02": assemblyLines.Add($"STAX    B"); break;
+                    case "00": assemblyLines.Add($"NOP"); break;
+                    case "01": assemblyLines.Add($"LXI    B,#${byte3}{byte2}"); break;
+                    case "02": assemblyLines.Add($"STAX   B"); break;
                     case "03": assemblyLines.Add($"INX    B"); break;
                     case "0F": assemblyLines.Add($"RRC"); break;
 
-                    case "21": assemblyLines.Add($"LXI    H,#${byteOffset2}{byteOffset1}"); break;
+                    case "21": assemblyLines.Add($"LXI    H,#${byte3}{byte2}"); break;
+                    case "20": assemblyLines.Add($"RIM"); break;
+                    case "27": assemblyLines.Add($"DAA"); break;
 
-                    case "32": assemblyLines.Add($"STA    ${byteOffset2}{byteOffset1}"); break;
-                    case "3E": assemblyLines.Add($"MVI    A,#${byteOffset1}"); break; // is byte2 or byte3?
+                    case "3A": assemblyLines.Add($"LDA    ${byte3}{byte2}"); break;
+                    case "32": assemblyLines.Add($"STA    ${byte3}{byte2}"); break;
+                    case "3E": assemblyLines.Add($"MVI    A,#${byte2}"); break; // is byte2 or byte3?
                     case "35": assemblyLines.Add($"DCR    M"); break;
 
+                    case "A7": assemblyLines.Add($"ANA    A"); break;
+                    case "AF": assemblyLines.Add($"XRA    A"); break;
 
+                    case "C3": assemblyLines.Add($"JMP    ${byte3}{byte2}"); break;
+                    case "C5": assemblyLines.Add($"PUSH   B"); break;
+                    case "CA": assemblyLines.Add($"JZ     ${byte3}{byte2}"); break;
+                    case "CD": assemblyLines.Add($"CALL   ${byte3}{byte2}"); break;
+                    case "C6": assemblyLines.Add($"ADI    #${byte2}"); break;
 
-
-
-
-                    case "C3": assemblyLines.Add($"JMP    ${byteOffset2}{byteOffset1}"); break;
-                    case "C5": assemblyLines.Add($"PUSH    B"); break;
-                    case "CD": assemblyLines.Add($"CALL    ${byteOffset2}{byteOffset1}"); break;
-
-                    case "D4": assemblyLines.Add($"CNC    ${byteOffset2}{byteOffset1}"); break;
+                    case "D4": assemblyLines.Add($"CNC    ${byte3}{byte2}"); break;
                     case "D5": assemblyLines.Add($"PUSH    D"); break;
-                    case "DA": assemblyLines.Add($"IN    #${byteOffset1}"); break;
-                    case "DB": assemblyLines.Add($"IN    #${byteOffset1}"); break;
+                    case "DA": assemblyLines.Add($"JC    ${byte3}{byte2}"); break;
+                    case "DB": assemblyLines.Add($"IN    #${byte2}"); break;
 
                     case "E5": assemblyLines.Add($"PUSH    H"); break;
+                    case "EB": assemblyLines.Add($"XCHG"); break;
 
                     case "F5": assemblyLines.Add($"PUSH    PSW"); break;
+                    case "FE": assemblyLines.Add($"CPI    #${byte2}"); break;
 
                     case "tuyj":
                         Console.WriteLine(5);
