8db82414fadd2bc4471ee3b4ec06afdae9a823ff
mihe <mihemihe@hotmail.com>
2017-12-18 15:21:35 +0400

    Added 8080 documentation. Added around 20 assmbly instructions to the disassembler.

--- mihemulator8080/CPU.cs
+++ mihemulator8080/CPU.cs
@@ -31,18 +31,43 @@ namespace mihemulator8080
             {
                 
                 string currentByte = BitConverter.ToString(bytesROM.Bytes, position, 1);
-                string byteOffset1 = BitConverter.ToString(bytesROM.Bytes, position + 1, 1);
+                string byteOffset1 = BitConverter.ToString(bytesROM.Bytes, position + 1, 1); //out of bound at the endfix
                 string byteOffset2 = BitConverter.ToString(bytesROM.Bytes, position + 2, 1);
 
 
                 switch (currentByte)
                 {
-                    // template: case "C1": assemblyLines.Add(""); break;
+                    // case "01": assemblyLines.Add($""); break;
                     // Opcode	Instruction	size	flags	function
-                    case "00": assemblyLines.Add("NOP"); break; // 0x00	NOP	1
-                    case "01": assemblyLines.Add("LXI    B,"); break;
-                    case "C3": assemblyLines.Add($"JMP    ${byteOffset2}{byteOffset1}"); break; // 0x00	NOP	1
+                    case "00": assemblyLines.Add($"NOP"); break; 
+                    case "01": assemblyLines.Add($"LXI    B,#${byteOffset2}{byteOffset1}"); break;
+                    case "02": assemblyLines.Add($"STAX    B"); break;
+                    case "03": assemblyLines.Add($"INX    B"); break;
+                    case "0F": assemblyLines.Add($"RRC"); break;
 
+                    case "21": assemblyLines.Add($"LXI    H,#${byteOffset2}{byteOffset1}"); break;
+
+                    case "32": assemblyLines.Add($"STA    ${byteOffset2}{byteOffset1}"); break;
+                    case "3E": assemblyLines.Add($"MVI    A,#${byteOffset1}"); break; // is byte2 or byte3?
+                    case "35": assemblyLines.Add($"DCR    M"); break;
+
+
+
+
+
+
+                    case "C3": assemblyLines.Add($"JMP    ${byteOffset2}{byteOffset1}"); break;
+                    case "C5": assemblyLines.Add($"PUSH    B"); break;
+                    case "CD": assemblyLines.Add($"CALL    ${byteOffset2}{byteOffset1}"); break;
+
+                    case "D4": assemblyLines.Add($"CNC    ${byteOffset2}{byteOffset1}"); break;
+                    case "D5": assemblyLines.Add($"PUSH    D"); break;
+                    case "DA": assemblyLines.Add($"IN    #${byteOffset1}"); break;
+                    case "DB": assemblyLines.Add($"IN    #${byteOffset1}"); break;
+
+                    case "E5": assemblyLines.Add($"PUSH    H"); break;
+
+                    case "F5": assemblyLines.Add($"PUSH    PSW"); break;
 
                     case "tuyj":
                         Console.WriteLine(5);
