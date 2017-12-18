faaef82049880547ec9be68437418f5a517d21f5
mihe <mihemihe@hotmail.com>
2017-12-18 13:02:53 +0400

    Adding ROMs.

--- mihemulator8080/CPU.cs
+++ mihemulator8080/CPU.cs
@@ -6,7 +6,30 @@ using System.Threading.Tasks;
 
 namespace mihemulator8080
 {
-    class CPU
+    public static class CPU
     {
+        public static byte registerA, registerB, registerC, registerD, registreE;
+
+        public static byte programCounter;
+
+        
+
+
+
+
+
+        public static String Assemble(string asmCode)
+        {
+
+            return "";
+        }
+
+        public static String Disassemble(string byteCode)
+        {
+
+            return "";
+        }
+
+
     }
 }
