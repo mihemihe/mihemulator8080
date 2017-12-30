57db50bceab06b6d45333c93a9caee3e8b2cbd8e
mihe <mihemihe@hotmail.com>
2017-12-30 18:08:20 +0400

    Issues implementing opcodes that set flags because I am used Byte for the registers, so they will roll over, but I need to have an extra bit to capture carry flag. Changing all code to unit fro byte.

--- mihemulator8080/CPU.cs
+++ mihemulator8080/CPU.cs
@@ -1,4 +1,4 @@
-﻿using System.Diagnostics;
+﻿using System;
 
 namespace mihemulator8080
 {
@@ -29,7 +29,12 @@ namespace mihemulator8080
         // However only 65535 values are required (2-16) to map all memory
         // if all memory is used it will fail !
         public static int programCounter; // (PC) An ancient Instruction Pointer
+
         public static int stackPointer; // (SP) Stack Pointer
+
+        public static int memoryAddressDE;
+        public static int memoryAddressHL;
+        public static byte[] tempBytesStorage;
 
         public static InstructionFetcher instructionFecther;
         public static string InstructionExecuting;
@@ -38,6 +43,9 @@ namespace mihemulator8080
         {
             instructionFecther = new InstructionFetcher();
             InstructionExecuting = "";
+            memoryAddressDE = 0;
+            memoryAddressHL = 0;
+            tempBytesStorage = new byte[4];
         }
 
         public static InstructionOpcodes GetNextInstruction()
@@ -45,15 +53,14 @@ namespace mihemulator8080
             // read the program counter
             // read next instruction frm membory, offset rogram counter
 
-
             if (programCounter < Memory.TextSectionSize)
             {
                 InstructionOpcodes codes = new InstructionOpcodes(
             Memory.RAMMemory[programCounter],
             Memory.RAMMemory[programCounter + 1],
             Memory.RAMMemory[programCounter + 2]);
-                InstructionExecuting =  CPU.instructionFecther.DisassembleInstruction(codes, out int size);
-                
+                InstructionExecuting = CPU.instructionFecther.DisassembleInstruction(codes, out int size);
+
                 programCounter += size;
                 return codes;
             }
@@ -62,57 +69,105 @@ namespace mihemulator8080
 
         public static int ExecuteInstruction(InstructionOpcodes opCodes)
         {
-
-
             switch (opCodes.Byte1)
             {
-
                 case 0x00: //NOP, do nothing
                     break;
+
                 case 0x01: //LXI    B,#${byte3}{byte2}
                     CPU.registerC = opCodes.Byte2;
                     CPU.registerB = opCodes.Byte3;
                     break;
-                case 0x05: //DCR B ************************************
+
+                case 0x05: //DCR B "Z, S, P, AC"
+                    uint result = 0;
+                    //////result = CPU.registerB - 1;
                     break;
+
                 case 0x06: //MVI    B,#${byte2}
                     CPU.registerB = opCodes.Byte2;
                     break;
+
                 case 0x11: //LXI    D,#${byte3}{byte2}
                     CPU.registerE = opCodes.Byte2;
                     CPU.registerD = opCodes.Byte3;
                     break;
+
+                case 0x13: //INX    D
+                    memoryAddressDE = 0;
+                    memoryAddressDE = CPU.registerD << 8;
+                    memoryAddressDE = memoryAddressDE | CPU.registerE;
+                    memoryAddressDE++;
+                    tempBytesStorage = BitConverter.GetBytes(memoryAddressDE);
+                    CPU.registerD = tempBytesStorage[1];
+                    CPU.registerE = tempBytesStorage[0];
+                    break;
+
+                case 0x1A: //LDAX   D - "A <- (DE)"
+                    memoryAddressDE = 0;
+                    memoryAddressDE = CPU.registerD << 8;
+                    memoryAddressDE = memoryAddressDE | CPU.registerE;
+                    CPU.registerA = Memory.RAMMemory[memoryAddressDE];
+
+                    break;
+
                 case 0x21: //LXI    H,#${byte3}{byte2}
                     CPU.registerL = opCodes.Byte2;
                     CPU.registerH = opCodes.Byte3;
                     break;
+
+                case 0x23: //INX    H
+                    memoryAddressHL = 0;
+                    memoryAddressHL = CPU.registerH << 8;
+                    memoryAddressHL = memoryAddressHL | CPU.registerL;
+                    memoryAddressHL++;
+                    tempBytesStorage = BitConverter.GetBytes(memoryAddressHL);
+                    CPU.registerH = tempBytesStorage[1];
+                    CPU.registerL = tempBytesStorage[0];
+                    break;
+
+                case 0x77: //MOV    M,A
+                    memoryAddressHL = 0;
+                    memoryAddressHL = CPU.registerH << 8;
+                    memoryAddressHL = memoryAddressHL | CPU.registerL;
+                    Memory.RAMMemory[memoryAddressHL] = CPU.registerA;
+
+                    break;
+
                 case 0xC3: //JMP    ${byte3}{byte2}
                     CPU.programCounter = opCodes.Byte3 << 8; //equal to byte3 + 8 bits padded right
-                    CPU.programCounter = CPU.programCounter | opCodes.Byte2; // fill the padded 8 bits right 
+                    CPU.programCounter = CPU.programCounter | opCodes.Byte2; // fill the padded 8 bits right
                     break;
+                case 0xC2: //JNZ    ${byte3}{byte2}
+                    // if z=false then jump to address
+                    if (CPU.ZeroFlag == false)
+                    {
+                        CPU.programCounter = opCodes.Byte3 << 8; //equal to byte3 + 8 bits padded right
+                        CPU.programCounter = CPU.programCounter | opCodes.Byte2; // fill the padded 8 bits right
+                    }
+                    break;
+
                 case 0x31: //LXI    SP,#${byte3}{byte2}
                     CPU.stackPointer = opCodes.Byte3 << 8;
                     CPU.stackPointer = CPU.stackPointer | opCodes.Byte2;
                     break;
+
                 case 0xCD: //CALL   ${byte3}{byte2}
                     // This is a PUSH to stack, fixed start address in the code, $2400
                     // for clarity , better of use returnaddress, but point is programCounter contains already pc  +2
                     //int returnAddress = CPU.programCounter; // no need +2 because it is incremented already
-                    Memory.RAMMemory[CPU.stackPointer - 1] = opCodes.Byte2; // is this the correct order? who knows 
+                    Memory.RAMMemory[CPU.stackPointer - 1] = opCodes.Byte2; // is this the correct order? who knows
                     Memory.RAMMemory[CPU.stackPointer - 2] = opCodes.Byte3;
                     CPU.stackPointer = CPU.stackPointer - 2;
                     CPU.programCounter = opCodes.Byte3 << 8; // This is a JMP
                     CPU.programCounter = CPU.programCounter | opCodes.Byte2;
                     break;
-                
-
 
                 default:
                     break;
             }
 
-
-                    return 0;
+            return 0;
         }
 
         public static int Cycle()
@@ -121,7 +176,6 @@ namespace mihemulator8080
             int cycleResult = ExecuteInstruction(nextInstruction);
             //Debug.Write(nextInstruction.Byte1 + "\n");
             return 0;
-
         }
     }
 }
